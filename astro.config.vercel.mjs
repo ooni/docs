9f8f4e64c0327584f518eedeee49f735a19e3126
Arturo Filastò <arturo@filasto.net>
2023-12-08 09:16:01 +0100

    Use static output for vercel

--- astro.config.vercel.mjs
+++ astro.config.vercel.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
 	integrations: [
 		starlight({
