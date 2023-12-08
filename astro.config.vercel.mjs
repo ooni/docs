2edb9aa3225d0846117556efb1f88144affff1ef
Arturo Filastò <arturo@filasto.net>
2023-12-08 09:05:10 +0100

    Support vercel and railway at the same time

---
+++ astro.config.vercel.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+	integrations: [
+		starlight({
+			title: 'OONI Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+      expressiveCode: false
+		}),
+	],
+});
