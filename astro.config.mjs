7e0408a9ed193bbb0c7fce9f78063f03063cecce
Arturo Filastò <arturo@filasto.net>
2024-07-04 20:55:25 +0200

    Re-enable expressive code and drop shikiji

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,47 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import vercel from "@astrojs/vercel/static";
-import rehypeShikiji from "rehype-shikiji";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [
-    starlight({
-      title: "OONI Docs",
-      social: {
-        github: "https://github.com/ooni/docs",
-      },
-      sidebar: [
-        {
-          label: "Data",
-          autogenerate: { directory: "data" },
-        },
-        {
-          label: "Meta",
-          autogenerate: { directory: "meta" },
-        },
-        {
-          label: "Devops",
-          autogenerate: { directory: "devops" },
-        },
-        {
-          label: "Backend",
-          autogenerate: { directory: "backend" },
-        },
-        {
-          label: "Probe Engine",
-          autogenerate: { directory: "probe-engine" },
-        },
-        {
-          label: "Legacy Backend",
-          autogenerate: { directory: "legacybackend" },
-        },
-      ],
-      expressiveCode: false,
-      customCss: [
-        './src/styles/custom.css',
-      ],
-    }),
-  ],
-  markdown: {
-    rehypePlugins: [[rehypeShikiji, { theme: "github-dark" }]],
-    syntaxHighlight: false,
-  },
+  integrations: [starlight({
+    title: "OONI Docs",
+    social: {
+      github: "https://github.com/ooni/docs"
+    },
+    sidebar: [{
+      label: "Data",
+      autogenerate: {
+        directory: "data"
+      }
+    }, {
+      label: "Meta",
+      autogenerate: {
+        directory: "meta"
+      }
+    }, {
+      label: "Devops",
+      autogenerate: {
+        directory: "devops"
+      }
+    }, {
+      label: "Backend",
+      autogenerate: {
+        directory: "backend"
+      }
+    }, {
+      label: "Probe Engine",
+      autogenerate: {
+        directory: "probe-engine"
+      }
+    }, {
+      label: "Legacy Backend",
+      autogenerate: {
+        directory: "legacybackend"
+      }
+    }],
+    customCss: ['./src/styles/custom.css']
+  })],
 });
