3186c83130a7c76be277ae242550f9e72e3d9dbc
Arturo Filastò <arturo@filasto.net>
2024-07-05 12:24:02 +0200

    Reflow astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,42 +6,51 @@ import vercel from "@astrojs/vercel/static";
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [starlight({
-    title: "OONI Docs",
-    social: {
-      github: "https://github.com/ooni/docs"
-    },
-    sidebar: [{
-      label: "Data",
-      autogenerate: {
-        directory: "data"
-      }
-    }, {
-      label: "Meta",
-      autogenerate: {
-        directory: "meta"
-      }
-    }, {
-      label: "Devops",
-      autogenerate: {
-        directory: "devops"
-      }
-    }, {
-      label: "Backend",
-      autogenerate: {
-        directory: "backend"
-      }
-    }, {
-      label: "Probe Engine",
-      autogenerate: {
-        directory: "probe-engine"
-      }
-    }, {
-      label: "Legacy Backend",
-      autogenerate: {
-        directory: "legacybackend"
-      }
-    }],
-    customCss: ['./src/styles/custom.css']
-  })],
+  integrations: [
+    starlight({
+      title: "OONI Docs",
+      social: {
+        github: "https://github.com/ooni/docs",
+      },
+      sidebar: [
+        {
+          label: "Data",
+          autogenerate: {
+            directory: "data",
+          },
+        },
+        {
+          label: "Meta",
+          autogenerate: {
+            directory: "meta",
+          },
+        },
+        {
+          label: "Devops",
+          autogenerate: {
+            directory: "devops",
+          },
+        },
+        {
+          label: "Backend",
+          autogenerate: {
+            directory: "backend",
+          },
+        },
+        {
+          label: "Probe Engine",
+          autogenerate: {
+            directory: "probe-engine",
+          },
+        },
+        {
+          label: "Legacy Backend",
+          autogenerate: {
+            directory: "legacybackend",
+          },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
